0acb6411a5708496fe8722271469bf980d14874d
sakshi <17june2001@gmail.com>
2022-09-05 06:48:32 +0530

    sounds

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,55 @@
+
+var numberOfDrums=document.querySelectorAll(".drum").length;
+
+for(var i = 0;i<numberOfDrums;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+
+    var buttonInnerHtml = this.innerHTML;
+    switch (buttonInnerHtml) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        
+        case "d":
+            var tom_1 = new Audio("sounds/tom-1.mp3");
+            tom_1.play();
+            break;
+
+        case "j":
+            var tom_2 = new Audio("sounds/tom-2.mp3");
+            tom_2.play();
+            break;
+
+        case "k":
+            var tom_3 = new Audio("sounds/tom-3.mp3");
+            tom_3.play();
+            break;
+
+        case "l":
+            var tom_4 = new Audio("sounds/tom-4.mp3");
+            tom_4.play();
+            break;
+
+        
+    
+        default: console.log(buttonInnerHtml);
+            
+    }
+});
+}
+
+
+ // alert("I got clicked!");
+//  var audio = new Audio("sounds/tom-1.mp3");
+//  audio.play();
